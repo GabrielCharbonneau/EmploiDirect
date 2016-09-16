54855cfcb652de885b59e059348ee64eadaf6a36
lebou <lebou@172.26.34.154>
2016-09-16 10:15:08 -0400

    Users connection

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,6 +43,13 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => ['username' => 'email', 'password' => 'password']
+                ]
+            ]
+        ]); // TODO ajuster la redirection du login/logout
     }
 
     /**
@@ -58,5 +65,9 @@ class AppController extends Controller
         ) {
             $this->set('_serialize', true);
         }
+    }
+    
+    public function beforeFilter(Event $event) {
+        $this->Auth->allow(['index', 'view', 'display']);
     }
 }
---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Users Model
+ *
+ * @property \Cake\ORM\Association\HasMany $Enterprises
+ *
+ * @method \App\Model\Entity\User get($primaryKey, $options = [])
+ * @method \App\Model\Entity\User newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\User[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\User|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\User[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\User findOrCreate($search, callable $callback = null)
+ */
+class UsersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('users');
+        $this->displayField('id');
+        $this->primaryKey('id');
+
+        $this->hasMany('Enterprises', [
+            'foreignKey' => 'user_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('email', 'create')
+            ->notEmpty('email');
+
+        $validator
+            ->requirePresence('password', 'create')
+            ->notEmpty('password');
+
+        $validator
+            ->requirePresence('role', 'create')
+            ->notEmpty('role')
+            ->add('role', 'inList', [
+                'rule' => ['inList', ['candidate', 'admin', 'enterprise']],
+                'message' => 'Please enter a valid role']);
+
+        return $validator;
+    }
+}
